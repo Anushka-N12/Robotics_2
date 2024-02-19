e6e84d801e572d3b3fe39b825ae1ff657fec0b22
Anushka-N12 <anushka.narsima@gmail.com>
2024-02-19 14:56:54 +0400

    Application with updated models

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, make_response, Response
+from ultralytics import YOLO
+import cam
+import cv2, json, os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+esp_cam = os.getenv('esp_cam')  
+
+app = Flask(__name__) 
+
+bin_model = YOLO("bin_best.pt")
+trash_model = YOLO("trash_best.pt")
+
+@app.route('/pred', methods=['GET'])
+def pred():
+    # Take image
+    print(cam.getimg(esp_cam))
+    # Run through custom model 
+    trash_results = trash_model('frame.jpg')
+    # Filter by size
+    objs = []
+    for result in trash_results:
+        result = result.cpu().boxes.numpy()
+        if len(result.cls) > 0:  # If detected, with decent confidence
+            # print(result)
+            [x1,y1,x2,y2] = result.xyxy[0]
+            og_shape = result.orig_shape
+
+            # # Confirm with pre-trained model
+            # passed = True
+            # image = cv2.imread("frame.jpg")
+            # cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)  # Draw a rectangle around obj
+            # cropped_image = image[y1:y2, x1:x2]    # Crop the image to the rectangle
+            # cv2.imwrite("cropped.jpg", cropped_image)    # Save in       
+            # cr_results = pt_model("cropped.jpg")
+            # for result in cr_results:
+            #     result = result.cpu().boxes.numpy()
+            #     if pt_classes[result.cls[0]] in ['person', 'bicycle', 'fire hydrant', 'stop sign', 'bench', 'bird', 'cat', 'dog', 'chair', 'potted plant', 'dining table']:
+            #         passed = False
+
+            # if passed == True and x2-x1 < og_shape[1]*0.75 and y2-y1 < og_shape[0]*0.75:  # Making sure obj is not too big
+            if True:
+                objs.append(((x2+x1)//2, (y2+y1)//2))  # Adding center coords of object to list
+
+    # Choose closest object
+    if len(objs) > 0:    
+        closest = (objs[0])
+        # ideal = (og_shape[1]//2, og_shape[0]*0.75)
+        for i in objs[1:]:
+            if i[1] < closest[1]:
+                closest = i
+        print(closest)
+    else:
+        closest = (-1, -1)
+
+    # Send result
+    # Create a JSON response and set custom header
+    d = {'coords': closest}
+    response = json.dumps([d])
+    response = Response(response, status=200, content_type='application/json')
+    response.headers['X-My-Header'] = 'foo'
+    return response, 200
+
+@app.route('/bin', methods=['GET'])
+def bin():
+    # Take image
+    print(cam.getimg(esp_cam))
+    # Run through custom model 
+    bin_results = bin_model('frame.jpg')
+    # Filter by size
+    bin = (-1,-1)
+    result = bin_results[0].cpu().boxes.numpy()
+    if len(result.cls) > 0:  # If detected, with decent confidence
+        # print(result)
+        [x1,y1,x2,y2] = result.xyxy[0]
+        og_shape = result.orig_shape
+        if True:
+            bin = (((x2+x1)//2, (y2+y1)//2))  # Adding center coords of object to list
+
+    # Send result
+    # Create a JSON response and set custom header
+    d = {'coords': bin}
+    response = json.dumps([d])
+    response = Response(response, status=200, content_type='application/json')
+    response.headers['X-My-Header'] = 'foo'
+    return response, 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # print(model.names)
+
+# results = model(r'C:\Users\anush\Projects\Emirates_Robotics\venv\data\test\trash.mp4', show=True)
---
+++ cam.py
@@ -0,0 +1,64 @@
+# Script to get & save images from url
+# https://www.digikey.com/en/maker/projects/esp32-cam-python-stream-opencv-example/840608badd0f4a5eb21b1be25ecb42cb
+
+# Importing required packages
+import cv2
+import numpy as np
+import requests
+
+def set_resolution(url: str, index: int=1, verbose: bool=False):
+    try:
+        if verbose:
+            resolutions = "10: UXGA(1600x1200)\n9: SXGA(1280x1024)\n8: XGA(1024x768)\n7: SVGA(800x600)\n6: VGA(640x480)\n5: CIF(400x296)\n4: QVGA(320x240)\n3: HQVGA(240x176)\n0: QQVGA(160x120)"
+            print("available resolutions\n{}".format(resolutions))
+
+        if index in [10, 9, 8, 7, 6, 5, 4, 3, 0]:
+            requests.get(url + "/control?var=framesize&val={}".format(index))
+        else:
+            print("Wrong index")
+    except:
+        print("SET_RESOLUTION: something went wrong")
+
+def set_quality(url: str, value: int=1, verbose: bool=False):
+    try:
+        if value >= 10 and value <=63:
+            requests.get(url + "/control?var=quality&val={}".format(value))
+    except:
+        print("SET_QUALITY: something went wrong")
+
+def set_awb(url: str, awb: int=1):
+    try:
+        awb = not awb
+        requests.get(url + "/control?var=awb&val={}".format(1 if awb else 0))
+    except:
+        print("SET_QUALITY: something went wrong")
+    return awb
+
+# Function used by app.py to get image
+def getimg(url):
+    cap = cv2.VideoCapture(url + ":81/stream")
+    set_resolution(url, index=8)
+    n,x = 0,[]
+    if True:
+        try:
+            if cap.isOpened():
+                cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
+                ret, frame = cap.read()
+                if ret:
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    gray = cv2.equalizeHist(gray)
+                n += 1
+                cv2.imwrite("frame.jpg", frame)  
+                # frame.set(5,640)
+                # frame.set(6,480)
+        except:
+            pass
+
+    # cv2.destroyAllWindows()
+    cap.release()
+    return 'Image saved.'
+
+# Test to execute only if this file is executed directly
+if __name__ == '__main__':
+    print(getimg('http://10.5.35.138'))
+
